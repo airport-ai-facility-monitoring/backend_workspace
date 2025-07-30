9a278c348af47cc9a9ebd2383d117710650ada93
munseunghwan2 <anstmdghks97@gmail.com>
2025-07-30 02:39:57 +0000

    test

--- member/src/main/java/airport/infra/EmployeeController.java
+++ member/src/main/java/airport/infra/EmployeeController.java
@@ -20,9 +20,10 @@ public class EmployeeController {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    @GetMapping("/employees/{id}")
-    public Optional<Employee> getEmployeeById(@PathVariable Long id) {
-        return employeeRepository.findById(id);
-    }
-}
+    // @GetMapping("/employees/{id}")
+    // public Optional<Employee> getEmployeeById(@PathVariable Long id) {
+    //      return employeeRepository.findById(id);
+
+    // }
+
 //>>> Clean Arch / Inbound Adaptor
