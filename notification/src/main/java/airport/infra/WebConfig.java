f45340c6d22a0cf48ca26225b602c3927b5d5e8c
gangmin522 <namgangmin522@gmail.com>
2025-08-11 00:21:14 +0000

    [수정] import 구문 추가

--- notification/src/main/java/airport/infra/WebConfig.java
+++ notification/src/main/java/airport/infra/WebConfig.java
@@ -3,6 +3,7 @@ package airport.infra;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.MediaType;
@@ -14,10 +15,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // allowCredentials(true)와 함께 사용 가능
+                .allowedOrigins("*") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 
     @Override
